11bc8e18962d3fb2c83e27465b47569ecf963feb
User <231583@astanait.edu.kz>
2023-12-24 18:15:00 +0600

    By longestSide() method, we will find the shape, which contains maximum size than other shapes

--- src/Shape.java
+++ src/Shape.java
@@ -14,4 +14,16 @@ public class Shape {
         }
         return p;
     }
+    public double longestSide(){
+        double max = 0;
+        for (int i = 0; i < points.size(); i++) {
+            for (int j = i + 1; j < points.size(); j++) {
+                double distance = points.get(i).distanceTo(points.get(j));
+                if (distance > max) {
+                    max = distance;
+                }
+            }
+        }
+        return max;
+    }
 }
