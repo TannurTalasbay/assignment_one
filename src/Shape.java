88dfce0221dc9cfe1e2ba0234aef4a86cbd10041
User <231583@astanait.edu.kz>
2023-12-24 18:10:23 +0600

    Here are created perimeter() method to find the sum of all shapes, which started and ended in the same point, for that case, we need to consider the size of arraylist, then iterate it. First, we have to get first point by index, then get the next point. After that, we call the method, where was found the shape size and add it and this will be continued until the end

--- src/Shape.java
+++ src/Shape.java
@@ -5,4 +5,13 @@ public class Shape {
     public Shape(ArrayList<Point> points){
         this.points = points;
     }
+    public double perimeter(){
+        double p = 0;
+        for(int i = 0; i < points.size(); i++){
+            Point current = points.get(i);
+            Point next = points.get((i + 1) % points.size());
+            p += current.distanceTo(next);
+        }
+        return p;
+    }
 }
