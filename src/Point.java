091d2ef3d524f6059fb5703359b63f408a37f977
User <231583@astanait.edu.kz>
2023-12-24 18:40:31 +0600

    Changed

--- src/Point.java
+++ src/Point.java
@@ -1,6 +1,6 @@
 public class Point {
-    private double x;
-    private double y;
+    private final double x;
+    private final double y;
 
     public Point(double x, double y) {
         this.x = x;
--- src/Shape.java
+++ src/Shape.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Shape {
-    private ArrayList<Point> points;
+    private final ArrayList<Point> points;
     public Shape(ArrayList<Point> points){
         this.points = points;
     }
